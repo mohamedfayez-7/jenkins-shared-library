f42443d545bc72ce16f9e2fcc51563f3478e98ad
Mohamed Fayez <mohamedfayez_7@yahoo.com>
2023-05-18 05:16:08 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,7 +2,7 @@
 def call(){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t $imageName ."
+        sh "docker build -t mohamedfayez7/my-repo:jma-3.0 ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push mohamedfayez7/my-repo:jma-3.0'
     }
