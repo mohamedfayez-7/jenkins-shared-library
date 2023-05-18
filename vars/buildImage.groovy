d28404cbb0a8593b838bcd71297fc48d907c254f
Mohamed Fayez <mohamedfayez_7@yahoo.com>
2023-05-18 05:40:24 +0200

    first commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+def call(string imageName){
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t $imageName ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push $imageName"
+    }
+}
